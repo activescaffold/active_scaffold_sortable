0dceb08e3e0fbfbaab8417d0438b5759618cf8b7
scambra <sergio@entrecables.com>
2010-03-26 17:24:36 +0100

    install assets script

--- init.rb
+++ init.rb
@@ -1,1 +1,11 @@
 require 'sortable'
+
+##
+## Run the install assets script, too, just to make sure
+## But at least rescue the action in production
+##
+begin
+  require File.dirname(__FILE__) + '/install_assets'
+rescue
+  raise $! unless Rails.env == 'production'
+end
