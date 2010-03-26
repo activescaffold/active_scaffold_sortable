637adb914e4bcbb36393bda83a613134c5c396bc
scambra <sergio@entrecables.com>
2010-03-26 17:40:43 +0100

    Fix for models which don't use acts_as_list

--- lib/active_scaffold/config/sortable.rb
+++ lib/active_scaffold/config/sortable.rb
@@ -3,12 +3,15 @@ module ActiveScaffold::Config
     def initialize(core_config)
       @options = {}
       @core = core_config
-      @column = core_config.columns[core_config.model.new.position_column]
+      self.column = core_config.model.new.position_column if @core.model.instance_methods.include? 'acts_as_list_class'
     end
 
     self.crud_type = :update
     
     attr_reader :column
+    def column=(column_name)
+      @column = @core.columns[column_name]
+    end
     
     attr_accessor :options
   end
