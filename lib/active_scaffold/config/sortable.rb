8e5ecfca2eae9495139151dcb683e6549d73123f
vhochstein <v.hochstein@highstone.de>
2010-09-24 14:54:37 +0200

    add error to log of position columns was nt found in model

--- lib/active_scaffold/config/sortable.rb
+++ lib/active_scaffold/config/sortable.rb
@@ -3,6 +3,7 @@ module ActiveScaffold::Config
     def initialize(core_config)
       @options = {}
       @core = core_config
+      
       self.column = core_config.model.new.position_column if @core.model.instance_methods.include? 'acts_as_list_class'
       self.column = core_config.model.new.left_column_name if @core.model.instance_methods.include? 'nested_set_scope'
     end
@@ -12,6 +13,8 @@ module ActiveScaffold::Config
     attr_reader :column
     def column=(column_name)
       @column = @core.columns[column_name]
+      Rails.logger.error("ActiveScaffold Sortable: postion column: #{column_name} not found in model") if @column.nil?
+      @column
     end
     
     attr_accessor :options
