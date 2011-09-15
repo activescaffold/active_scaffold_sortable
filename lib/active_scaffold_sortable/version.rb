bbe38e7f6cd79ff58eb40b63ebe08725f7609921
scambra <sergio@entrecables.com>
2011-09-15 11:19:24 +0200

    update version

--- lib/active_scaffold_sortable/version.rb
+++ lib/active_scaffold_sortable/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldSortable
   module Version
     MAJOR = 3
-    MINOR = 0
-    PATCH = 2
+    MINOR = 1
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
