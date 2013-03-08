802d608ef237ba875aae07dc71eb92e60f9a3f1c
Sergio Cambra <sergio@programatica.es>
2013-03-08 13:55:21 +0100

    fix sortable without url, bump to 3.2.2

--- app/assets/javascripts/jquery/active_scaffold_sortable.js
+++ app/assets/javascripts/jquery/active_scaffold_sortable.js
@@ -23,14 +23,16 @@ ActiveScaffold.sortable = function(element) {
       ActiveScaffold.update_positions(content);
     };
   } else {
-    var csrf = jQuery('meta[name=csrf-param]').attr('content') + '=' + jQuery('meta[name=csrf-token]').attr('content');
     var url = element.data('reorder-url');
-    sortable_options.update = function(event, ui) {
-      var body = jQuery(this).sortable('serialize',{key: encodeURIComponent(jQuery(this).attr('id') + '[]'), expression: new RegExp(element.data('format'))});
-      var params = element.data('with');
-      if (params) body += '&' + params;
-      jQuery.post(url, body + '&' + csrf);
-    };
+    if (url) {
+      var csrf = jQuery('meta[name=csrf-param]').attr('content') + '=' + jQuery('meta[name=csrf-token]').attr('content');
+      sortable_options.update = function(event, ui) {
+        var body = jQuery(this).sortable('serialize',{key: encodeURIComponent(jQuery(this).attr('id') + '[]'), expression: new RegExp(element.data('format'))});
+        var params = element.data('with');
+        if (params) body += '&' + params;
+        jQuery.post(url, body + '&' + csrf);
+      };
+    }
   }
   sortable_options.handle = element.data('handle');
   sortable_options.items = element.data('tag');
@@ -40,7 +42,7 @@ ActiveScaffold.sortable = function(element) {
 jQuery(document).ready(function($) {
   $(document).on('as:action_success', 'a.as_action', function(e, action_link) {
     var sortable = $('.sortable-container', action_link.adapter);
-    if (sortable.length) $.each(sortable, function(i, s) { ActiveScaffold.sortable(s); });
+    if (sortable.length) $.each(sortable, function(i, s) { ActiveScaffold.sortable($(s)); });
   });
   var sortable = $('.sortable-container');
   if (sortable.length) ActiveScaffold.sortable(sortable);
