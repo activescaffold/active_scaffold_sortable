59c6329436006930d591572ac895a1fd1b96e0cb
Sergio Cambra <sergio@programatica.es>
2015-09-15 13:06:09 +0200

    not include reorder_url on subforms, not needed and it can broke if parent controller has no route for reorder

--- lib/active_scaffold_sortable/view_helpers.rb
+++ lib/active_scaffold_sortable/view_helpers.rb
@@ -3,10 +3,10 @@ module ActiveScaffoldSortable
     def sort_params(config = active_scaffold_config)
       options = {
         :tag => 'tr', 
-        :reorder_url => url_for(params_for(:action => :reorder, :controller => controller_name)),
         :format => '^[^_-](?:[A-Za-z0-9_-]*)-(.*)-row$',
         :column => config.sortable.column.name
       }
+      options[:reorder_url] = url_for(params_for(:action => :reorder, :controller => controller_name)) if config == self.active_scaffold_config
       options.merge config.sortable.options
     end
 
