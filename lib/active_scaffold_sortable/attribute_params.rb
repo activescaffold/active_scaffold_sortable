507fb9a41a6382cc6f6232290c9d2214c4358376
Sergio Cambra <sergio@programatica.es>
2015-09-30 10:00:47 +0200

    add missing file

---
+++ lib/active_scaffold_sortable/attribute_params.rb
@@ -0,0 +1,18 @@
+module ActiveScaffoldSortable
+  module AttributeParams
+    def self.included(base)
+      base.alias_method_chain :update_column_from_params, :sortable
+    end
+
+    def update_column_from_params_with_sortable(parent_record, column, attribute, avoid_changes = false)
+      update_column_from_params_without_sortable(parent_record, column, attribute, avoid_changes).tap do |value|
+        if column.plural_association?
+          config = active_scaffold_config_for(column.association.klass)
+          if config.actions.include?(:sortable)
+            parent_record.association(column.association.name).target = value.sort_by(&config.sortable.column.name)
+          end
+        end
+      end
+    end
+  end
+end
