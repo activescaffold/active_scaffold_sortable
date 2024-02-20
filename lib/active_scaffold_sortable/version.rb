55bca23d7dc66d1edf4eb7bc3a92c8fbd02e9552
Sergio Cambra <sergio@enpijama.es>
2024-02-20 13:20:11 +0100

    bump to 3.4.0

--- lib/active_scaffold_sortable/version.rb
+++ lib/active_scaffold_sortable/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldSortable
   module Version
     MAJOR = 3
-    MINOR = 3
-    PATCH = 10
+    MINOR = 4
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
