f2464d210b02b44e2124b09f5cc4cdeef01ab441
Sergio Cambra <sergio@enpijama.es>
2024-05-07 12:35:39 +0200

    use ActiveScaffold hook on initialization

--- lib/active_scaffold_sortable.rb
+++ lib/active_scaffold_sortable.rb
@@ -8,19 +8,3 @@ module ActiveScaffoldSortable
   autoload 'ViewHelpers', 'active_scaffold_sortable/view_helpers.rb'
   autoload 'AttributeParams', 'active_scaffold_sortable/attribute_params.rb'
 end
-
-module ActiveScaffold
-  module Actions
-    ActiveScaffold.autoload_subdir('actions', self, File.dirname(__FILE__))
-  end
-
-  module Config
-    ActiveScaffold.autoload_subdir('config', self, File.dirname(__FILE__))
-  end
-
-  module Helpers
-    ActiveScaffold.autoload_subdir('helpers', self, File.dirname(__FILE__))
-  end
-end
-ActiveScaffold.stylesheets << 'active_scaffold_sortable'
-ActiveScaffold.javascripts << 'active_scaffold_sortable'
