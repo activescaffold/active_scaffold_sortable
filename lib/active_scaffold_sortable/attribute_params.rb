27a7470ee6d67b4c01dc16c7e7a7627b96870a5c
Sergio Cambra <sergio@enpijama.es>
2026-02-24 10:44:02 +0100

    ensure sortable column has value in every record before sorting

--- lib/active_scaffold_sortable/attribute_params.rb
+++ lib/active_scaffold_sortable/attribute_params.rb
@@ -5,7 +5,10 @@ module ActiveScaffoldSortable
         if column.association.try(:collection?)
           config = active_scaffold_config_for(column.association.klass)
           if config.actions.include?(:sortable)
-            parent_record.association(column.association.name).target = value.sort_by(&config.sortable.column.name)
+            sortable_col = config.sortable.column.name
+            max_value = value.map(&sortable_col).compact.max
+            value.each { |record| record.send("#{sortable_col}=", max_value += 1) unless record.send(sortable_col) }
+            parent_record.association(column.association.name).target = value.sort_by(&sortable_col)
           end
         end
       end
